5d46eadb438e43b4c17ea612941b7fbc031e90f3
samarthh22 <sam22arthgupta@gmail.com>
2026-09-04 20:16:08 +0530

    Time: 25 ms (83.37%), Space: 80.4 MB (13.29%) - LeetHub

---
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int n = g.size();
+        int m = s.size();
+        int i = 0, j = 0;
+        while(i < n && j < m) {
+            if(s[j] >= g[i]) {
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+};
